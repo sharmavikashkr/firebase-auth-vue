45c731280ff8931603afecdadab946c349056599
Vikash Kumar <sharma.vikashkr@gmail.com>
2022-02-08 14:13:09 +0530

    Firebase Auth Vue

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+export {
+    auth
+};
